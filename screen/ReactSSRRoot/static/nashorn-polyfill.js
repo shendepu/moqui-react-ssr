7dda3d5fb8254c8c5bb611bc10d9483b034565e7
SHEN, JIMMY <shendepu@hotmail.com>
2017-01-14 19:18:01 +0800

    Updated nashorn-polyfill which has setImmediate and clearImmediate polyfill

--- screen/ReactSSRRoot/static/nashorn-polyfill.js
+++ screen/ReactSSRRoot/static/nashorn-polyfill.js
@@ -18,16 +18,17 @@ console.trace = print;
 (function nashornEventLoopMain(context) {
   'use strict';
 
-  var TimerTask =  Java.type("java.util.TimerTask");
   var TimeUnit = Java.type("java.util.concurrent.TimeUnit");
+  var Runnable = Java.type('java.lang.Runnable');
   var HashMap = Java.type('java.util.HashMap');
 
 //   var finalException = null;
 
-  var timerMap = new HashMap(1);
+  var timerMap = new HashMap();
   var globalTimerId = 1;
 
-  var timer = context.__NASHORN_POLYFILL_TIMER__;
+  // __NASHORN_POLYFILL_TIMER__ type is ScheduledExecutorService
+  var scheduler = context.__NASHORN_POLYFILL_TIMER__;
 
   var onTaskFinished = function () {
   };
@@ -36,24 +37,26 @@ console.trace = print;
 //     timer.cancel();
 //   };
 
-  function createTimerTask (fn, timerId, args, repeated) {
-    return new TimerTask {
+  function createRunnable (fn, timerId, args, repeated) {
+    return new Runnable {
       run: function () {
         try {
           fn.apply(context, args);
           if (!repeated) timerMap.remove(timerId);
         } catch (e) {
-//           console.error(e);
-//           console.trace(e);
-//           console.trace(fn);
-//           console.trace(args);
-//           console.trace(repeated);
+          console.error(e);
+          console.trace(e);
+          console.trace(fn);
+          console.trace(args);
+          console.trace(repeated);
           // Clear the task
-          if (repeated) {
-            var task = timerMap.get(timerId);
-            if (task) task.cancel();
-          }
+//           if (repeated) {
+          var task = timerMap.get(timerId);
+          if (task) task.cancel();
+//           }
           timerMap.remove(timerId);
+        } finally {
+          if (!repeated) timerMap.remove(timerId);
         }
       }
     }
@@ -63,14 +66,22 @@ console.trace = print;
     var args = [].slice.call(arguments, 2, arguments.length);
 
     var timerId = globalTimerId++;
-    var task = createTimerTask(fn, timerId, args, false);
+    var runnable = createRunnable(fn, timerId, args, false);
 
+    var task = scheduler.schedule(runnable, millis, TimeUnit.MILLISECONDS);
     timerMap.put(timerId, task);
-
-    timer.schedule(task, millis, TimeUnit.MILLISECONDS);
 
     return timerId;
   };
+
+  var setImmediate = function (fn /* [, args...] */) {
+    var args = [].slice.call(arguments, 1, arguments.length);
+    return setTimeout(fn, 0, args);
+  }
+
+  var clearImmediate = function (timerId) {
+    clearTimeout(timerId);
+  }
 
   var clearTimeout = function (timerId) {
     var task = timerMap.get(timerId);
@@ -84,10 +95,9 @@ console.trace = print;
     var args = [].slice.call(arguments, 2, arguments.length);
 
     var timerId = globalTimerId++;
-    var task = createTimerTask(fn, timerId, args, true);
+    var runnable = createRunnable(fn, timerId, args, true);
+    var task = scheduler.scheduleWithFixedDelay(runnable, delay, delay, TimeUnit.MILLISECONDS);
     timerMap.put(timerId, task);
-
-    timer.scheduleWithFixedDelay(task, delay, delay, TimeUnit.MILLISECONDS);
 
     return timerId;
   };
@@ -98,6 +108,8 @@ console.trace = print;
 
   context.setTimeout = setTimeout;
   context.clearTimeout = clearTimeout;
+  context.setImmediate = setImmediate;
+  context.clearImmediate = clearImmediate;
   context.setInterval = setInterval;
   context.clearInterval = clearInterval;
   context.__nashorn_polyfill_timerMap = timerMap;
